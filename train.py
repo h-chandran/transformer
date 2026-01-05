5f7971a09fc7fd86e1ac718980b500585b83f41b
Harish <72057623+h-chandran@users.noreply.github.com>
2026-01-05 14:37:41 -0600

    limited data analysis

--- train.py
+++ train.py
@@ -7,3 +7,12 @@ Currently, we are using the tiny shakespeare dataset, which is a text file conta
 with open('input.txt', 'r', encoding = 'utf-8') as f:
     data = f.read()
 
+# Some limited data analysis to understand the dataset.
+
+print("length of dataset in characters: ", len(data))
+print(data[:1000])
+
+chars = sorted(list(set(data)))
+vocab_size = len(chars)
+print("all the unique characters: ", ''.join(chars))
+print("vocab size: ", vocab_size)
