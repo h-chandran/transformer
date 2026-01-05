49adeb54186388ffee91623412ef81eeca8f2b61
Harish <72057623+h-chandran@users.noreply.github.com>
2026-01-05 15:20:11 -0600

    template encoder and decoder

--- train.py
+++ train.py
@@ -16,3 +16,14 @@ chars = sorted(list(set(data)))
 vocab_size = len(chars)
 print("all the unique characters: ", ''.join(chars))
 print("vocab size: ", vocab_size)
+
+# mapping from characters to integers and vice versa
+stoi = { ch:i for i, ch in enumerate(chars) }
+itos = { i:ch for i, ch in enumerate(chars) }
+def encode(s):
+    return [stoi[c] for c in s]
+def decode(l):
+    return ''.join([itos[i] for i in l])
+
+print("encode: ", encode("hii there"))
+print("decode: ", decode(encode("hii there")))
