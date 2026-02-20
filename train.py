8a190e69fdd948f6f03d8c3d38e28a0ce70177f4
Harish <72057623+h-chandran@users.noreply.github.com>
2026-02-20 12:55:44 -0500

    asd

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ Input File
 
 Currently, we are using the tiny shakespeare dataset, which is a text file containing the works of Shakespeare. 
 
-"""
+"""    
 with open('input.txt', 'r', encoding = 'utf-8') as f:
     text = f.read()
 
